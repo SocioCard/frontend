c6e36522ae7e87f0eae09b1bd1e88767a407cb2d
Tushar11Raj <tushar11raj@gmail.com>
2021-06-30 12:15:59 +0530

    homepage

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -4,7 +4,10 @@ import Footer from "../components/footer";
 import { Grid, CssBaseline, Button, Paper } from "@material-ui/core";
 import { makeStyles } from '@material-ui/core/styles';
 import hero from '../static/images/hero.svg'
+import allinone from '../static/images/allinone.svg'
 import logo from '../static/images/logo.png'
+import linksPage from '../static/images/linksPage.png'
+import profilePage from '../static/images/profilePage.png'
 import { classes } from "istanbul-lib-coverage";
 
 const useStyles = makeStyles((theme) => ({
@@ -31,6 +34,15 @@ const useStyles = makeStyles((theme) => ({
         width: '95%',
         margin: '40px 0 30px 0',
     },
+    h2: {
+        color: '#22406b',
+        width: '80%',
+        margin: '40px 10px 30px 10px',
+        textAlign: 'center',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        maxWidth: '400px'
+    },
     hero: {
         //maxWidth: '900px',
         display: 'block',
@@ -53,7 +65,20 @@ const useStyles = makeStyles((theme) => ({
         },
     },
     lower: {
-        backgroundColor: '#fff'
+        backgroundColor: '#fff',
+        margin: '100px 0'
+    },
+    allinone: {
+        //maxWidth: '900px',
+        display: 'block',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        width: '50%',
+    },
+    featuresImage:{
+        width: '100%',
+        margin: '20px',
+        maxWidth: '200px'
     },
 }))
 
@@ -76,7 +101,7 @@ export default function Homepage(){
             className={classes.main}
             >
                 <Grid item xs={12} md={7}>
-                <h1 className={classes.h1}>The only link you will ever need! <br/>Your universal profile.</h1>        
+                <h1 className={classes.h1}>The only link you will ever need!</h1>        
                 </Grid>
                 <Grid item xs={12} md={5}>
                 <h3 className={classes.h3}>This is your social card. Add links, cards, banners and more. Choose the theme with which you vibe. <br/>Your own universal profile page!</h3>
@@ -84,8 +109,48 @@ export default function Homepage(){
                 </Grid>
             </Grid>
             <img className={classes.hero} src={hero} />
+            <h1 className={classes.h2}>Let your SocioCard speak for you. Use this as your your universal bio!</h1>
             <div className={classes.lower}>
+                <Grid
+                container
+                direction="column"
+                justify="center"
+                alignItems="center"
+                >
+                    <Grid container
+                    direction="row"
+                    justify="center"
+                    alignItems="center"
+                    xs={12} md={8}>
+                        <Grid item xs={8} md={6}>
+                        <img className={classes.featuresImage} src={linksPage}/>
+                        </Grid>
+                        <Grid item xs={9} md={6}>
+                        <div>
+                            <h3>Various Elements to add</h3>
+                            <p>Add button links, cards style links and icon links. Add buymeacoffee link to get support from followers!</p>
+                        </div>
+                        </Grid>                          
+                    </Grid>
+                    <Grid container
+                    direction="row"
+                    justify="center"
+                    alignItems="center"
+                    xs={12} md={8}>
+                        <Grid item xs={8} md={6}>
+                        <img className={classes.featuresImage} src={profilePage}/>
+                        </Grid>
+                        <Grid item xs={9} md={6}>
+                        <div>
+                            <h3>Personalised Page</h3>
+                            <p>Add name, and a  bio to describe yourself.</p>
+                        </div>
+                        </Grid>                          
+                    </Grid>
+                </Grid>
             </div>
+            <img src={allinone} className={classes.allinone}/>
+            <h2 className={classes.h2}>All your links in one place!</h2>
             <SignIn/>
             <Footer/>
         </Grid>
